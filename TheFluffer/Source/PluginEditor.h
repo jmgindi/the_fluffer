205e94b0432453bbdec84d4ebe8fa589518067f8
Jack Gindi <jmgindi@gmail.com>
2019-11-13 11:16:22 -0800

    doesn't build yet, figuring out deprecation issues

---
+++ TheFluffer/Source/PluginEditor.h
@@ -0,0 +1,47 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin editor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+//==============================================================================
+/**
+*/
+class NewProjectAudioProcessorEditor  : public AudioProcessorEditor
+{
+public:
+    NewProjectAudioProcessorEditor (NewProjectAudioProcessor&);
+    ~NewProjectAudioProcessorEditor();
+
+    //==============================================================================
+    void paint (Graphics&) override;
+    void resized() override;
+
+private:
+    
+    ScopedPointer<Slider> driveKnob;
+    ScopedPointer<Slider> rangeKnob;
+    ScopedPointer<Slider> blendKnob;
+    ScopedPointer<Slider> volumeKnob;
+    
+    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> driveAttachment;
+    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> rangeAttachment;
+    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> blendAttachment;
+    ScopedPointer<AudioProcessorValueTreeState::SliderAttachment> volumeAttachment;
+
+    
+    // This reference is provided as a quick way for your editor to
+    // access the processor object that created it.
+    NewProjectAudioProcessor& processor;
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (NewProjectAudioProcessorEditor)
+};
